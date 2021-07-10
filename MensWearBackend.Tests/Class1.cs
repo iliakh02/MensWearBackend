01c10b8cf86ac20d3f4f95f35b4803a7f49e8e0b
Liakh Ivan <liakhivan21@gmail.com>
2021-07-10 15:48:52 +0300

    Add project files.

---
+++ MensWearBackend.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace MensWearBackend.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
